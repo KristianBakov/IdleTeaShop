52e175bbe2504d930c9c42cb345ae3c79103bc9a
KristianBakov <kristian.bakov@yahoo.com>
2024-02-17 20:40:48 +0000

    Refactored furniture factory code

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -2,25 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TestScript : MonoBehaviour
 {
-    [SerializeField]
-    private List<GameObject> _tabls;
+     [SerializeField]
+    private List<GameObject> tabls;
     
     [SerializeField]
-    private List<Sprite> _tablsSprites;
+    private List<Sprite> tablsSprites;
     
     private void Start()
     {
         BasicFurnitureFactory basicFurnitureFactory = new BasicFurnitureFactory();
-        foreach (var table in _tabls)
+        foreach (var table in tabls)
         {
             IFurniture furniture = basicFurnitureFactory.CreateTable(table, new Vector3(2,1,20));
             furniture.PrintType();
         }
         
-        foreach (var table in _tablsSprites)
+        foreach (var table in tablsSprites)
         {
             IFurniture furniture = basicFurnitureFactory.CreateTable(table, Vector3.zero, Quaternion.identity);
             furniture.PrintType();
